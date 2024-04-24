808370beccf08cc88011004d7a40d78744def8c2
AK <akrivit@gmail.com>
2024-04-23 20:58:23 -0700

    weird docker bug

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 gcloud config set run/region us-central1
-docker build -t express-proxy .
+# docker build -t express-proxy .
+docker buildx build --platform linux/amd64 -t express-proxy .
 docker tag express-proxy gcr.io/mixpanel-gtm-training/express-proxy
 docker push gcr.io/mixpanel-gtm-training/express-proxy
 gcloud run deploy express-proxy \
@@ -7,5 +8,5 @@ gcloud run deploy express-proxy \
   --platform managed \
   --project mixpanel-gtm-training \
   --allow-unauthenticated \
-  --set-env-vars MIXPANEL_TOKEN=none,FRONTEND_URL=none
+  --set-env-vars MIXPANEL_TOKEN=none,FRONTEND_URL=none,RUNTIME=prod,REGION=US
 
